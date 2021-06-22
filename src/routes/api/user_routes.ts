ebdc8691ec667f484ec9999c203b0767756a5d33
MaxR522 <ranjamario@gmail.com>
2021-06-22 09:20:43 +0300

    👌 IMPROVE: add more validation update info

--- src/routes/api/user_routes.ts
+++ src/routes/api/user_routes.ts
@@ -7,6 +7,7 @@ import UpdateInfo from '../../controllers/user_controller/update_info';
 import verifyAccessToken from '../../middlewares/verify_token';
 import authValidationFor from '../../middlewares/auth_field_validators';
 import checkValidationResult from '../../middlewares/check_field_validation';
+import blacklistedAccessCheck from '../../middlewares/blacklist_access_token';
 
 // Init Router
 const route = Router();
@@ -16,6 +17,7 @@ route.patch(
   authValidationFor('user_info'),
   checkValidationResult,
   verifyAccessToken,
+  blacklistedAccessCheck,
   UpdateInfo,
 );
 
